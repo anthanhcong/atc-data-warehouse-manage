4c8be53e29d9183e82e938ea10c94cd69c5818bc
ptl211a4bk@gmail.com <ptl211a4bk@gmail.com@4c6077c4-ce17-81f8-0f77-a2ecad5fbf6e>
2014-06-24 12:06:20 +0000

    + Edit Stock import and Warehouse List

--- Trunk/WarehouseManage/WarehouseManage/WHM_STOCK/Stock_Manage_Function.cs
+++ Trunk/WarehouseManage/WarehouseManage/WHM_STOCK/Stock_Manage_Function.cs
@@ -19,12 +19,13 @@ namespace WarehouseManager
 {
     public partial class Form1
     {
-        const int Stock_Manage_COL_NUM = 6;
+        const int Stock_Manage_COL_NUM = 7;
         ExcelImportStruct[] Stock_Manage_Col = new ExcelImportStruct[Stock_Manage_COL_NUM];
-        const int Stock_Manage_WH_ID = 0;
-        const int Stock_Manage_Part_Number = 1;
-        const int Stock_Manage_Bin = 2;
-        const int Stock_Manage_Plant = 3;
+        const int Stock_Manage_Ma_LH = 0;
+        const int Stock_Manage_WH_ID = 1;
+        const int Stock_Manage_Part_Number = 2;
+        const int Stock_Manage_Bin = 3;
+        const int Stock_Manage_Plant = 4;
 
         public DataTable Stock_Manage_Tbl;
         public DataSet Stock_Manage_Tbl_ds = new DataSet();
@@ -34,12 +35,13 @@ namespace WarehouseManager
 
         private void Stock_Manage_InitExcelCol_Infor()
         {
-            Stock_Manage_Col[0] = new ExcelImportStruct("WareHouse_ID", "WareHouse_ID", Excel_Col_Type.COL_STRING, 30);
-            Stock_Manage_Col[1] = new ExcelImportStruct("Part_Number", "Part_Number", Excel_Col_Type.COL_STRING, 50);
-            Stock_Manage_Col[2] = new ExcelImportStruct("Bin", "Bin", Excel_Col_Type.COL_STRING, 50);
-            Stock_Manage_Col[3] = new ExcelImportStruct("Plant", "Plant", Excel_Col_Type.COL_STRING, 50);
-            Stock_Manage_Col[4] = new ExcelImportStruct("Qty", "Qty", Excel_Col_Type.COL_FLOAT, 10);
-            Stock_Manage_Col[5] = new ExcelImportStruct("Description", "Description", Excel_Col_Type.COL_STRING, 200);
+            Stock_Manage_Col[0] = new ExcelImportStruct("Ma_loai_hinh", "Ma_loai_hinh", Excel_Col_Type.COL_STRING, 20);
+            Stock_Manage_Col[1] = new ExcelImportStruct("WareHouse_ID", "WareHouse_ID", Excel_Col_Type.COL_STRING, 30);
+            Stock_Manage_Col[2] = new ExcelImportStruct("Part_Number", "Part_Number", Excel_Col_Type.COL_STRING, 50);
+            Stock_Manage_Col[3] = new ExcelImportStruct("Bin", "Bin", Excel_Col_Type.COL_STRING, 50);
+            Stock_Manage_Col[4] = new ExcelImportStruct("Plant", "Plant", Excel_Col_Type.COL_STRING, 50);
+            Stock_Manage_Col[5] = new ExcelImportStruct("Qty", "Qty", Excel_Col_Type.COL_FLOAT, 10);
+            Stock_Manage_Col[6] = new ExcelImportStruct("Description", "Description", Excel_Col_Type.COL_STRING, 200);
         }
 
         private bool Stock_Manage_Get_Col_info(Excel.Workbook cur_wbook)
@@ -94,8 +96,9 @@ namespace WarehouseManager
         private bool Import_Stock_Manage_Table_in_file(string file_name)
         {
             int row;
-            string part_number, wh_id, bin, plant;
-            bool exist_wh_id = false, exist_part = false;
+            string part_number, wh_id, bin, plant, ma_lh;
+            bool exist_wh_id = false, exist_part = false, ret_save = true;
+            string import_allow;
             string cell_str;
 
 
@@ -117,60 +120,100 @@ namespace WarehouseManager
                     bin = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, Stock_Manage_Col[Stock_Manage_Bin].Col, Stock_Manage_Col[Stock_Manage_Bin].Data_Max_len);
                     plant = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, Stock_Manage_Col[Stock_Manage_Plant].Col, Stock_Manage_Col[Stock_Manage_Plant].Data_Max_len);
                     wh_id = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, Stock_Manage_Col[Stock_Manage_WH_ID].Col, Stock_Manage_Col[Stock_Manage_WH_ID].Data_Max_len);
+                    ma_lh = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, Stock_Manage_Col[Stock_Manage_Ma_LH].Col, Stock_Manage_Col[Stock_Manage_Ma_LH].Data_Max_len);
                     // Kiem tra Line da co trong database chua
-                    exist_wh_id = Is_exist_WH_ID(wh_id);
-                    exist_part  = Is_exist_Part_number(part_number);
+                    import_allow = Allow_import(ma_lh, wh_id);
+                    if (import_allow == "Y")
+                    {
+                        exist_wh_id = Is_exist_WH_ID(wh_id);
+                        exist_part = Is_exist_Part_number(part_number);
 
-                    if (exist_part == false)
-                    {
-                        Error_log += "\nPart number: " + part_number + " is'nt exist in Material list Datatable.";
-                    }
-                    if (exist_wh_id == false)
-                    {
-                        Error_log += "\nWarehouse ID: " + wh_id + " is'nt exist in Warehouse List Datatable.";
-                    }
-                    if ((exist_part == true) && (exist_wh_id == true))
-                    {
-                        if (Is_exist_Stock_Manage(part_number, bin, plant, wh_id) == true)
+                        if (exist_part == false)
                         {
-                            // Update for this row
-                            Update_Stock_Manage_Line(part_number, bin, plant, wh_id, (Excel.Worksheet)OpenWB.Sheets[1], row);
+                            Error_log += "\nPart number: " + part_number + " is'nt exist in Material list Datatable.";
                         }
-                        else
+                        if (exist_wh_id == false)
                         {
-                            // Insert new row
-                            Create_New_Stock_Manage_Line(part_number, bin, plant, wh_id, (Excel.Worksheet)OpenWB.Sheets[1], row);
+                            Error_log += "\nWarehouse ID: " + wh_id + " is'nt exist in Warehouse List Datatable.";
                         }
+                        if ((exist_part == true) && (exist_wh_id == true))
+                        {
+                            if (Is_exist_Stock_Manage(part_number, bin, plant, wh_id) == true)
+                            {
+                                // Update for this row
+                                Update_Stock_Manage_Line(part_number, bin, plant, wh_id, (Excel.Worksheet)OpenWB.Sheets[1], row);
+                            }
+                            else
+                            {
+                                // Insert new row
+                                Create_New_Stock_Manage_Line(part_number, bin, plant, wh_id, (Excel.Worksheet)OpenWB.Sheets[1], row);
+                            }
+                        }
+                        row++;
+                        cell_str = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, 2, 20);
+                        ProgressBar1.Value = row % 100;
+                        StatusLabel.Text = "Loading File, Line: " + row.ToString();
                     }
-                    row++;
-                    cell_str = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, 2, 20);
-                    ProgressBar1.Value = row % 100;
-                    StatusLabel.Text = "Loading File, Line: " + row.ToString();
-                }
-
-                Close_WorkBook(OpenWB);
-                // Store data
-                if (Error_log != "")
-                {
-                    MessageBox.Show(Error_log, "Error");
-                    ProgressBar1.Visible = false;
-                    StatusLabel.Text = "DONE";
-                }
-                else
-                {
-                    if (Update_SQL_Data(Stock_Table_Form.Data_da, Stock_Table_Form.Data_dtb) == true)
+                    else if ((import_allow == "N") || (import_allow == ""))
                     {
-                        ProgressBar1.Visible = false;
-                        StatusLabel.Text = "DONE";
-                        MessageBox.Show("Complete Import Data");
+                        Error_log = "Please check 'Import_allow' of 'Ma_loai_hinh':" + ma_lh + " and 'WareHouse_ID': " + wh_id
+                                                            + " if you want to import";
+                        //MessageBox.Show("Please check 'Import_allow' of 'Ma_loai_hinh':" + ma_lh+ " and 'WareHouse_ID': " + wh_id 
+                        //                                    + " if you want to import", "Error");
+                        row++;
+                        cell_str = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, 2, 20);
+                        ProgressBar1.Value = row % 100;
+                        StatusLabel.Text = "Loading File, Line: " + row.ToString();
+                        ret_save = false;
+                        //break;
                     }
                     else
                     {
+                        Error_log = "Please import info 'Ma_loai_hinh':" + ma_lh + " and 'WareHouse_ID': " + wh_id
+                                                            + " with WH_List_and_Material_List Tab";
+                        //MessageBox.Show("Please import info 'Ma_loai_hinh':" + ma_lh+ " and 'WareHouse_ID': " + wh_id 
+                        //                                    + " with WH_List_and_Material_List Tab", "Error");
+                        row++;
+                        cell_str = Get_Text_Cell((Excel.Worksheet)OpenWB.Sheets[1], row, 2, 20);
+                        ProgressBar1.Value = row % 100;
+                        StatusLabel.Text = "Loading File, Line: " + row.ToString();
+                        ret_save = false;
+                        //break;
+                    }
+                }
+                Close_WorkBook(OpenWB);
+                // Store data
+                if (ret_save == true)
+                {
+                    if (Error_log != "")
+                    {
+                        MessageBox.Show(Error_log, "Error");
                         ProgressBar1.Visible = false;
-                        StatusLabel.Text = "Import Failed";
-                        MessageBox.Show("Import Data Failed");
                         StatusLabel.Text = "DONE";
                     }
+                    else
+                    {
+                        if (Update_SQL_Data(Stock_Table_Form.Data_da, Stock_Table_Form.Data_dtb) == true)
+                        {
+                            ProgressBar1.Visible = false;
+                            StatusLabel.Text = "DONE";
+                            MessageBox.Show("Complete Import Data");
+                        }
+                        else
+                        {
+                            ProgressBar1.Visible = false;
+                            StatusLabel.Text = "Import Failed";
+                            MessageBox.Show("Import Data Failed");
+                            StatusLabel.Text = "DONE";
+                        }
+                    }
+                }
+                else
+                {
+                    ProgressBar1.Visible = false;
+                    MessageBox.Show(Error_log, "Error");
+                    StatusLabel.Text = "Import Failed";
+                    StatusLabel.Text = "DONE";
                 }
             }
             else
@@ -210,6 +253,24 @@ namespace WarehouseManager
                 }
             }
             return false;
+        }
+
+        private string Allow_import(string ma_loaihinh, string warehouse_id)
+        {
+            string cur_ma_lh, cur_wh_id, cur_sub_wh_id, im_allow;
+            string ret_val = "Nulldata";
+            foreach (DataRow row in Load_WH_ID_List_Tbl.Rows)
+            {
+                cur_ma_lh = row["Ma_loai_hinh"].ToString().Trim();
+                cur_wh_id = row["WareHouse_ID"].ToString().Trim();
+                cur_sub_wh_id = row["Sub_wh_id"].ToString().Trim();
+                im_allow = row["Import_allow"].ToString().Trim();
+                if ((cur_ma_lh == ma_loaihinh) && (cur_wh_id == warehouse_id))
+                {
+                    ret_val = im_allow;
+                }
+            }
+            return ret_val;
         }
 
         private void Load_Material_List()
