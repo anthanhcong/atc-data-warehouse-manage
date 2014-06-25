9a49ec2efac8fa9eef55f04a3578839c389e1754
ptl211a4bk@gmail.com <ptl211a4bk@gmail.com@4c6077c4-ce17-81f8-0f77-a2ecad5fbf6e>
2014-06-25 12:35:11 +0000

    + Edit Form Warehouse Dashboard and function.

---
+++ Trunk/WarehouseManage/WarehouseManage/WHM_STOCK/Warehouse_Dashboard/Warehouse_Dashboard_EventHandle.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Ports;
+using Microsoft.Office.Core;
+using System.Diagnostics;
+using System.Threading;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WarehouseManager
+{
+    public partial class Form1
+    {
+        private void WH_Daskboard_Create_BT_Click(object sender, EventArgs e)
+        {
+            string cur_wh_id, wh_id, wh_name, mother_wh, note;
+            int import_allow = 0;
+            //DataRow newrow = WH_Daskboard_Table_Form.Data_dtb.NewRow();
+
+            wh_id = WH_Daskboard_WH_ID_TbxL.My_TextBox.Text.ToString().Trim();
+            wh_name = WH_Daskboard_WH_Name_TbxL.My_TextBox.Text.ToString().Trim();
+            mother_wh = WH_Daskboard_Mother_WH_CbxL.My_Combo.Text.ToString().Trim();
+            if (WH_Daskboard_Check_Import.My_CheckBox.Checked == true)
+            {
+                import_allow = 1;
+            }
+            note = WH_Daskboard_Note_Txt_Lb.My_TextBox.Text.ToString().Trim();
+            if (wh_id == "")
+            {
+                MessageBox.Show("Please select WH ID", "Error");
+                return;
+            }
+            else if (wh_name == "")
+            {
+                MessageBox.Show("Please select WH Name", "Error");
+                return;
+            }
+            foreach (DataRow row in WH_Daskboard_Table_Form.Data_dtb.Rows)
+            {
+                cur_wh_id = row["WareHouse_ID"].ToString().Trim();
+                if (cur_wh_id == wh_id)
+                {
+                    DialogResult thongbao;
+
+                    thongbao = (MessageBox.Show("WH ID was created\n"
+                                                + "WH ID = " + wh_id 
+                                                + "Do you want to update ?", " Attention ",
+                                                            MessageBoxButtons.YesNo, MessageBoxIcon.Warning));
+                    if (thongbao == DialogResult.Yes)
+                    {
+                        row["WareHouse_ID"] = wh_id;
+                        row["WareHouse_Name"] = wh_name;
+                        row["Import_allow"] = import_allow;
+                        row["Mother_WHID"] = mother_wh;
+                        row["Note"] = note;
+                        WH_Daskboard_Store_BT_Click(null, null);
+                    }
+                    return;
+                }
+            }
+            DataRow newrow = WH_Daskboard_Table_Form.Data_dtb.NewRow();
+            newrow["WareHouse_ID"] = wh_id;
+            newrow["WareHouse_Name"] = wh_name;
+            newrow["Import_allow"] = import_allow;
+            newrow["Mother_WHID"] = mother_wh;
+            newrow["Note"] = note;
+            WH_Daskboard_Table_Form.Data_dtb.Rows.Add(newrow);
+        }
+
+        private void WH_Daskboard_Store_BT_Click(object sender, EventArgs e)
+        {
+            if (Update_SQL_Data(WH_Daskboard_Table_Form.Data_da, WH_Daskboard_Table_Form.Data_dtb) == true)
+            {
+                MessageBox.Show("Store Data Complete", "Successful");
+            }
+            else
+            {
+                MessageBox.Show("Store Data Fail", "Failed");
+            }
+        }
+
+        private void WH_ID_with_MLH_Create_BT_Click(object sender, EventArgs e)
+        {
+            string cur_wh_id, cur_ma_lh, id, ma_lh, wh_id, im_or_ex, ty_le;
+            decimal tyle;
+
+            id = WH_ID_with_MaLH_ID_TbxL.My_TextBox.Text.ToString().Trim();
+            ma_lh = WH_ID_with_MaLH_Ma_LH_TbxL.My_TextBox.Text.ToString().Trim();
+            wh_id = WH_ID_with_MaLH_WH_ID_CbxL.My_Combo.Text.ToString().Trim();
+            im_or_ex = WH_ID_with_MaLH_Im_or_Ex_TbxL.My_TextBox.Text.ToString().Trim();
+            ty_le = WH_ID_with_MaLH_Ty_le_TbxL.My_TextBox.Text.ToString().Trim();
+            //if (id == "")
+            //{
+            //    MessageBox.Show("Please select ID", "Error");
+            //    return;
+            //}else
+            if (ma_lh == "")
+            {
+                MessageBox.Show("Please select 'Mã loại hình'", "Error");
+                return;
+            }
+            else if (im_or_ex == "")
+            {
+                MessageBox.Show("Please select 'Import/Export'", "Error");
+                return;
+            }
+            if (!isNumeric(ty_le, System.Globalization.NumberStyles.Number))
+            {
+                MessageBox.Show("Please enter a valid 'Tỷ lệ %'", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            foreach (DataRow row in WH_ID_with_MaLH_Table_Form.Data_dtb.Rows)
+            {
+                cur_wh_id = row["WareHouse_ID"].ToString().Trim();
+                cur_ma_lh = row["Ma_loai_hinh"].ToString().Trim();
+                if ((cur_wh_id == wh_id) && (cur_ma_lh == ma_lh))
+                {
+                    DialogResult thongbao;
+
+                    thongbao = (MessageBox.Show("Item was created\n"
+                                                + "Mã loại hình = " + ma_lh
+                                                + "WH ID = " + wh_id
+                                                + "Do you want to update ?", " Attention ",
+                                                            MessageBoxButtons.YesNo, MessageBoxIcon.Warning));
+                    if (thongbao == DialogResult.Yes)
+                    {
+                        row["ID"] = id;
+                        row["Ma_loai_hinh"] = ma_lh;
+                        row["WareHouse_ID"] = wh_id;
+                        row["Import_Export"] = im_or_ex;
+                        row["Ty_le"] = decimal.Parse(ty_le);
+                        WH_Daskboard_Store_BT_Click(null, null);
+                    }
+                    return;
+                }
+            }
+            tyle = decimal.Parse(ty_le);
+            DataRow newrow = WH_ID_with_MaLH_Table_Form.Data_dtb.NewRow();
+            newrow["ID"] = id;
+            newrow["Ma_loai_hinh"] = ma_lh;
+            newrow["WareHouse_ID"] = wh_id;
+            newrow["Import_Export"] = im_or_ex;
+            newrow["Ty_le"] = tyle;
+            WH_ID_with_MaLH_Table_Form.Data_dtb.Rows.Add(newrow);
+        }
+
+        private void WH_ID_with_MLH_Store_BT_Click(object sender, EventArgs e)
+        {
+            if (Update_SQL_Data(WH_ID_with_MaLH_Table_Form.Data_da, WH_ID_with_MaLH_Table_Form.Data_dtb) == true)
+            {
+                MessageBox.Show("Store Data Complete", "Successful");
+            }
+            else
+            {
+                MessageBox.Show("Store Data Fail", "Failed");
+            }
+        }
+        
+    }
+}
