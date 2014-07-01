048b6680504ef81eb3ad0d57bf92ebcc045a8d64
ptl211a4bk@gmail.com <ptl211a4bk@gmail.com@4c6077c4-ce17-81f8-0f77-a2ecad5fbf6e>
2014-07-01 12:19:31 +0000

    + Add form Mother WH Stock

---
+++ trunk/Trunk/WarehouseManage/WarehouseManage/WHM_STOCK/Mother_WH_Stock/Mother_WH_Stock_EventHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Ports;
+using Microsoft.Office.Core;
+using System.Diagnostics;
+using System.Threading;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WarehouseManager
+{
+    public partial class Form1
+    {
+        private void Mother_Stock_Table_Form_CellDoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mother_Stock_Part_Number_cbx_SelectedValueChanged(Object sender, EventArgs e)
+        { }
+        
+        private void Mother_Stock_WH_ID_List_cbx_SelectedValueChanged(Object sender, EventArgs e)
+        { }
+
+        private void Mother_Stock_Part_Number_cbx_KeyDown(object sender, KeyEventArgs e)
+        { }
+
+        private void Mother_Stock_WH_ID_List_cbx_KeyDown(object sender, KeyEventArgs e)
+        { }
+
+        private void Mother_Stock_Bin_Txt_KeyDown(object sender, KeyEventArgs e)
+        { }
+
+        private void Mother_Stock_Plant_Txt_KeyDown(object sender, KeyEventArgs e)
+        { }
+
+        private void Mother_Stock_Search_BT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mother_Stock_Store_BT_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
